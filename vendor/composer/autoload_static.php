3f9a4029a0f0b1e13391fa71b41d3d36e243b839
adrian-morelos <ing.adrianmorelos@gmail.com>
2018-12-18 21:07:39 -0500

    Deploy

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -132,6 +132,7 @@ class ComposerStaticInitDrupal8
         array (
             'Composer\\Semver\\' => 16,
             'Composer\\Installers\\' => 20,
+            'CommerceGuys\\Addressing\\' => 24,
         ),
         'B' => 
         array (
@@ -445,6 +446,10 @@ class ComposerStaticInitDrupal8
         'Composer\\Installers\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+        'CommerceGuys\\Addressing\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/commerceguys/addressing/src',
         ),
         'Behat\\Mink\\Driver\\' => 
         array (
